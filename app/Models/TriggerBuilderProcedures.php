824c1559229e8b367377b8aa22f6d3d7cbc731b7
Your Name <you@example.com>
2025-06-10 17:32:12 +0700

    Triggers

---
+++ app/Models/TriggerBuilderProcedures.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Models;
+
+class TriggerBuilderProcedures
+{
+    public static function procLogMenuUpdate()
+    {
+        return <<<SQL
+CREATE PROCEDURE log_menu_update(
+    IN p_m_id CHAR(36), IN p_old_name VARCHAR(60), IN p_new_name VARCHAR(60),
+    IN p_old_price DECIMAL(12,2), IN p_new_price DECIMAL(12,2),
+    IN p_old_stock INT, IN p_new_stock INT
+)
+BEGIN
+    INSERT INTO menu_update_logs (m_id, old_name, new_name, old_price, new_price, old_stock, new_stock)
+    VALUES (p_m_id, p_old_name, p_new_name, p_old_price, p_new_price, p_old_stock, p_new_stock);
+END;
+SQL;
+    }
+
+    public static function procLogTransactionStatus()
+    {
+        return <<<SQL
+CREATE PROCEDURE log_transaction_status(
+    IN p_t_id CHAR(36), IN p_old_status VARCHAR(60), IN p_new_status VARCHAR(60)
+)
+BEGIN
+    INSERT INTO transaction_status_logs (t_id, old_status, new_status)
+    VALUES (p_t_id, p_old_status, p_new_status);
+END;
+SQL;
+    }
+
+    public static function procUpdateDailyRevenue()
+    {
+        return <<<SQL
+CREATE PROCEDURE proc_update_daily_revenue()
+BEGIN
+    REPLACE INTO daily_revenue_logs (log_date, total_revenue)
+    SELECT DATE(t_time), SUM(t_total)
+    FROM transactions
+    WHERE t_status = 'Selesai'
+    GROUP BY DATE(t_time);
+END;
+SQL;
+    }
+
+    public static function procUpdateMonthlyRevenue()
+    {
+        return <<<SQL
+CREATE PROCEDURE proc_update_monthly_revenue()
+BEGIN
+    REPLACE INTO monthly_revenue_logs (log_month, total_revenue)
+    SELECT DATE_FORMAT(t_time, '%Y-%m'), SUM(t_total)
+    FROM transactions
+    WHERE t_status = 'Selesai'
+    GROUP BY DATE_FORMAT(t_time, '%Y-%m');
+END;
+SQL;
+    }
+
+    public static function procReduceUserPoints()
+    {
+        return <<<SQL
+CREATE PROCEDURE proc_reduce_user_points(
+    IN p_user_id CHAR(36), IN p_discount DECIMAL(12,2)
+)
+BEGIN
+    UPDATE users SET point = point - FLOOR(p_discount) WHERE id = p_user_id;
+    INSERT INTO user_points_logs (user_id, change_amount, event)
+    VALUES (p_user_id, -FLOOR(p_discount), 'discount_applied');
+END;
+SQL;
+    }
+
+    public static function procAddUserPoints()
+    {
+        return <<<SQL
+CREATE PROCEDURE proc_add_user_points(
+    IN p_user_id CHAR(36)
+)
+BEGIN
+    UPDATE users SET point = point + 1 WHERE id = p_user_id;
+    INSERT INTO user_points_logs (user_id, change_amount, event)
+    VALUES (p_user_id, 1, 'transaction_complete');
+END;
+SQL;
+    }
+
+    public static function procReduceStock()
+    {
+        return <<<SQL
+    CREATE PROCEDURE proc_reduce_stock(IN p_t_id CHAR(36))
+    BEGIN
+        UPDATE menus m
+        JOIN transaction_details td ON td.m_id = m.m_id
+        SET m.m_stock = m.m_stock - td.td_quantity
+        WHERE td.t_id = p_t_id;
+    END;
+    SQL;
+    }
+    
+
+    public static function procAddVendorEarnings()
+    {
+        return <<<SQL
+CREATE PROCEDURE proc_add_vendor_earnings(
+    IN p_t_id CHAR(36)
+)
+BEGIN
+    INSERT INTO vendor_earnings_logs (vendor_id, amount)
+    SELECT m.v_id, SUM(m.m_price * td.td_quantity)
+    FROM transaction_details td
+    JOIN menus m ON m.m_id = td.m_id
+    WHERE td.t_id = p_t_id
+    GROUP BY m.v_id;
+END;
+SQL;
+    }
+}
+
---
+++ database/migrations/2025_06_09_145756_create_log_tables_and_triggers.php
@@ -0,0 +1,105 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use App\Models\TriggerBuilderTables;
+use App\Models\TriggerBuilderProcedures;
+use App\Models\TriggerBuilderTriggers;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        // Tables
+        DB::statement(TriggerBuilderTables::createMenuUpdateLogTable());
+        DB::statement(TriggerBuilderTables::createTransactionStatusLogTable());
+        DB::statement(TriggerBuilderTables::createDailyRevenueLogTable());
+        DB::statement(TriggerBuilderTables::createMonthlyRevenueLogTable());
+        DB::statement(TriggerBuilderTables::createUserPointsLogTable());
+        DB::statement(TriggerBuilderTables::createVendorEarningsLogTable());
+
+        // Procedures
+        DB::statement('DROP PROCEDURE IF EXISTS log_menu_update');
+        DB::statement(TriggerBuilderProcedures::procLogMenuUpdate());
+
+        DB::statement('DROP PROCEDURE IF EXISTS log_transaction_status');
+        DB::statement(TriggerBuilderProcedures::procLogTransactionStatus());
+
+        DB::statement('DROP PROCEDURE IF EXISTS proc_update_daily_revenue');
+        DB::statement(TriggerBuilderProcedures::procUpdateDailyRevenue());
+
+        DB::statement('DROP PROCEDURE IF EXISTS proc_update_monthly_revenue');
+        DB::statement(TriggerBuilderProcedures::procUpdateMonthlyRevenue());
+
+        DB::statement('DROP PROCEDURE IF EXISTS proc_reduce_user_points');
+        DB::statement(TriggerBuilderProcedures::procReduceUserPoints());
+
+        DB::statement('DROP PROCEDURE IF EXISTS proc_add_user_points');
+        DB::statement(TriggerBuilderProcedures::procAddUserPoints());
+
+        DB::statement('DROP PROCEDURE IF EXISTS proc_reduce_stock');
+        DB::statement(TriggerBuilderProcedures::procReduceStock());
+
+        DB::statement('DROP PROCEDURE IF EXISTS proc_add_vendor_earnings');
+        DB::statement(TriggerBuilderProcedures::procAddVendorEarnings());
+
+        // Triggers
+        DB::statement('DROP TRIGGER IF EXISTS tr_after_menu_update');
+        DB::statement(TriggerBuilderTriggers::triggerAfterMenuUpdate());
+
+        DB::statement('DROP TRIGGER IF EXISTS tr_after_transaction_status');
+        DB::statement(TriggerBuilderTriggers::triggerAfterTransactionStatusChange());
+
+        DB::statement('DROP TRIGGER IF EXISTS tr_after_transaction_insert_daily');
+        DB::statement(TriggerBuilderTriggers::triggerAfterTransactionInsertDaily());
+
+        DB::statement('DROP TRIGGER IF EXISTS tr_after_transaction_insert_monthly');
+        DB::statement(TriggerBuilderTriggers::triggerAfterTransactionInsertMonthly());
+
+        DB::statement('DROP TRIGGER IF EXISTS tr_after_transaction_discount');
+        DB::statement(TriggerBuilderTriggers::triggerAfterTransactionDiscount());
+
+        DB::statement('DROP TRIGGER IF EXISTS tr_after_transaction_add_points');
+        DB::statement(TriggerBuilderTriggers::triggerAfterTransactionAddPoints());
+
+        DB::statement('DROP TRIGGER IF EXISTS tr_after_transaction_vendor_earnings');
+        DB::statement(TriggerBuilderTriggers::triggerAfterTransactionVendorEarnings());
+
+        // Trigger
+        DB::statement('DROP TRIGGER IF EXISTS tr_after_transaction_reduce_stock');
+        DB::statement(TriggerBuilderTriggers::triggerAfterTransactionReduceStock());
+
+    }
+
+    public function down(): void
+    {
+        // Drop triggers
+        DB::statement('DROP TRIGGER IF EXISTS tr_after_menu_update');
+        DB::statement('DROP TRIGGER IF EXISTS tr_after_transaction_status');
+        DB::statement('DROP TRIGGER IF EXISTS tr_after_transaction_insert_daily');
+        DB::statement('DROP TRIGGER IF EXISTS tr_after_transaction_insert_monthly');
+        DB::statement('DROP TRIGGER IF EXISTS tr_after_transaction_discount');
+        DB::statement('DROP TRIGGER IF EXISTS tr_after_transaction_add_points');
+        DB::statement('DROP TRIGGER IF EXISTS tr_after_transaction_vendor_earnings');
+        DB::statement('DROP TRIGGER IF EXISTS tr_after_transaction_reduce_stock');
+
+        // Drop procedures
+        DB::statement('DROP PROCEDURE IF EXISTS log_menu_update');
+        DB::statement('DROP PROCEDURE IF EXISTS log_transaction_status');
+        DB::statement('DROP PROCEDURE IF EXISTS proc_update_daily_revenue');
+        DB::statement('DROP PROCEDURE IF EXISTS proc_update_monthly_revenue');
+        DB::statement('DROP PROCEDURE IF EXISTS proc_reduce_user_points');
+        DB::statement('DROP PROCEDURE IF EXISTS proc_add_user_points');
+        DB::statement('DROP PROCEDURE IF EXISTS proc_reduce_stock');
+        DB::statement('DROP PROCEDURE IF EXISTS proc_add_vendor_earnings');
+
+        // Drop tables
+        Schema::dropIfExists('menu_update_logs');
+        Schema::dropIfExists('transaction_status_logs');
+        Schema::dropIfExists('daily_revenue_logs');
+        Schema::dropIfExists('monthly_revenue_logs');
+        Schema::dropIfExists('user_points_logs');
+        Schema::dropIfExists('vendor_earnings_logs');
+    }
+};
