eafc6cf98f6743cb5059bae09cd02e0dea343865
kev1nandreas <kevin.andreascn@gmail.com>
2025-06-11 23:14:47 +0700

    Add error handling for database timezone setting in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Exception;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,7 +23,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        DB::statement("SET time_zone = '+07:00'");
+        try {
+            DB::statement("SET time_zone = '+07:00'");
+        } catch (Exception $e) {
+            Log::error('Failed to set database timezone: ' . $e->getMessage());
+        }
 
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
